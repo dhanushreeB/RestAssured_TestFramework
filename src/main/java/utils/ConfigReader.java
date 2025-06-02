831f4a2b6580cf019a376bb4f9269eb076645316
Dhanushree <bdhanushree862@gmail.com>
2025-06-02 23:18:45 +0530

    Second commit

---
+++ src/main/java/utils/ConfigReader.java
@@ -0,0 +1,21 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+public class ConfigReader {
+    private static Properties prop = new Properties();
+
+    static {
+        try {
+            FileInputStream fis = new FileInputStream("src/test/resources/config.properties");
+            prop.load(fis);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String getProperty(String key) {
+        return prop.getProperty(key);
+    }
+}
